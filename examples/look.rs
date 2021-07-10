64c661e32752483742d67f93954b0cca984908ff
Marko Lazić <marko.lazic@comyno.com>
2021-07-11 00:20:18 +0200

    Remove query for player

--- examples/look.rs
+++ examples/look.rs
@@ -5,40 +5,43 @@ use bevy_prototype_debug_lines::{DebugLines, DebugLinesPlugin};
 
 fn main() {
     App::build()
+        .init_resource::<Game>()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(DebugLinesPlugin)
         .add_plugin(PickingPlugin)
         .add_startup_system(setup.system())
-        .add_system(rotate_object.system())
+        .add_system(player_look_at_hit_point.system())
         .run();
 }
 
-fn rotate_object(
-    mut player_transform_query: Query<&mut Transform, With<Player>>,
+fn player_look_at_hit_point(
+    game: Res<Game>,
     mut lines: ResMut<DebugLines>,
-    picking_camera_query: Query<&PickingCamera>,
+    pickings: Query<&PickingCamera>,
+    mut transforms: Query<&mut Transform>,
 ) {
-    for mut player_transform in player_transform_query.iter_mut() {
-        for raycast_source in picking_camera_query.iter() {
-            match raycast_source.intersect_top() {
-                Some(top_intersection) => {
-                    let transform_new = top_intersection.1.normal_ray().to_transform();
-                    let mouse_world = Transform::from_matrix(transform_new);
-                    let hit_point =
-                        Ray3d::new(player_transform.translation, mouse_world.translation);
-                    let aim_point =
-                        Vec3::new(hit_point.direction().x, 0.5, hit_point.direction().z);
-                    player_transform.look_at(aim_point, Vec3::Y);
-                    lines.line(hit_point.origin(), hit_point.direction(), 0.);
-                }
-                None => {}
+    for raycast_source in pickings.iter() {
+        match raycast_source.intersect_top() {
+            Some(top_intersection) => {
+                let transform_new = top_intersection.1.normal_ray().to_transform();
+                let mouse_world = Transform::from_matrix(transform_new);
+                let mut player_transform = game.player.transform;
+                let hit_point = Ray3d::new(player_transform.translation, mouse_world.translation);
+                let aim_point = Vec3::new(hit_point.direction().x, 0.5, hit_point.direction().z);
+
+                player_transform.look_at(aim_point, Vec3::Y);
+                *transforms.get_mut(game.player.entity.unwrap()).unwrap() = player_transform;
+
+                lines.line(hit_point.origin(), hit_point.direction(), 0.);
             }
+            None => {}
         }
     }
 }
 
 fn setup(
+    mut game: ResMut<Game>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -68,15 +71,27 @@ fn setup(
         // Designate the camera as our source
         .insert_bundle(PickingCameraBundle::default());
 
-    // seeker
-    commands
-        .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..Default::default()
-        })
-        .insert(Player);
+    game.player.transform = Transform::from_xyz(0.0, 0.5, 0.0);
+    // player
+    game.player.entity = Some(
+        commands
+            .spawn_bundle(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                transform: game.player.transform,
+                ..Default::default()
+            })
+            .id(),
+    );
 }
 
-struct Player;
+#[derive(Default)]
+struct Player {
+    entity: Option<Entity>,
+    transform: Transform,
+}
+
+#[derive(Default)]
+struct Game {
+    player: Player,
+}
