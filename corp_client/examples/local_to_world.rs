ca83ef59ca72015c1e0d9474a7b470e2539ddb0e
Lazić <markolazic@protonmail.com>
2022-03-30 22:00:27 +0200

    Add world to local

--- corp_client/examples/local_to_world.rs
+++ corp_client/examples/local_to_world.rs
@@ -11,6 +11,8 @@ fn main() {
         .add_startup_system(setup_system)
         .add_system(move_parent)
         .add_system(move_child)
+        .add_system(move_world)
+        .add_system(update_world_to_local)
         .run();
 }
 
@@ -23,17 +25,49 @@ fn move_parent(
     parent_tr.translation.y = data.parent.y;
 }
 
-fn move_child(mut child_query: Query<&mut Transform, With<ChildPoint>>, data: Res<InspectorData>) {
+fn move_child(
+    mut child_query: Query<&mut Transform, With<ChildPoint>>,
+    parent_query: Query<&Transform, (With<ParentPoint>, Without<ChildPoint>)>,
+    data: Res<InspectorData>,
+) {
     let mut child_tr = Option::unwrap(child_query.iter_mut().last());
-    let world_space_point = local_to_world(data.child, data.parent);
+    let parent_tr = Option::unwrap(parent_query.iter().last());
+
+    let world_space_point = local_to_world(data.child, &parent_tr);
     child_tr.translation = Vec3::new(world_space_point.x, world_space_point.y, 0.0);
 }
 
-fn local_to_world(local_pt: Vec2, global_pt: Vec2) -> Vec2 {
-    let right = Vec2::X;
-    let up = Vec2::Y;
+fn move_world(mut world_query: Query<&mut Transform, With<WorldPoint>>, data: Res<InspectorData>) {
+    let mut world_tr = Option::unwrap(world_query.iter_mut().last());
+    world_tr.translation.x = data.world.x;
+    world_tr.translation.y = data.world.y;
+}
+
+fn update_world_to_local(
+    world_query: Query<&Transform, With<WorldPoint>>,
+    child_query: Query<&Transform, (With<ChildPoint>, Without<WorldPoint>)>,
+    mut data: ResMut<InspectorData>,
+) {
+    let child_tr = Option::unwrap(child_query.iter().last());
+    let world_tr = Option::unwrap(world_query.iter().last());
+    let to_local = world_to_local(child_tr, world_tr.translation);
+    data.world_to_local = to_local;
+}
+
+fn local_to_world(local_pt: Vec2, parent_pt: &Transform) -> Vec3 {
+    let right = parent_pt.right();
+    let up = parent_pt.up();
     let world_offset = right * local_pt.x + up * local_pt.y;
-    global_pt + world_offset
+    parent_pt.translation + world_offset
+}
+
+fn world_to_local(child_pt: &Transform, world_pt: Vec3) -> Vec2 {
+    let right = child_pt.right();
+    let up = child_pt.up();
+    let relative_pt = world_pt - child_pt.translation;
+    let x = relative_pt.dot(right);
+    let y = relative_pt.dot(up);
+    Vec2::new(x, y)
 }
 
 fn setup_system(mut commands: Commands) {
@@ -74,12 +108,31 @@ fn setup_system(mut commands: Commands) {
             Transform::from_xyz(100.0, 50.0, 0.0),
         ))
         .insert(ChildPoint);
+
+    let square = shapes::RegularPolygon {
+        sides: 4,
+        feature: shapes::RegularPolygonFeature::Radius(10.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &square,
+            DrawMode::Outlined {
+                fill_mode: FillMode::color(Color::RED),
+                outline_mode: StrokeMode::new(Color::BLACK, 1.0),
+            },
+            Transform::from_xyz(100.0, -150.0, 0.0),
+        ))
+        .insert(WorldPoint);
 }
 
 #[derive(Inspectable)]
 struct InspectorData {
     parent: Vec2,
     child: Vec2,
+    world: Vec2,
+    world_to_local: Vec2,
 }
 
 #[derive(Component)]
@@ -88,11 +141,16 @@ struct ParentPoint;
 #[derive(Component)]
 struct ChildPoint;
 
+#[derive(Component)]
+struct WorldPoint;
+
 impl Default for InspectorData {
     fn default() -> InspectorData {
         InspectorData {
             parent: Default::default(),
             child: Vec2::new(100.0, 50.0),
+            world: Vec2::new(100.0, -150.0),
+            world_to_local: Vec2::new(100.0, -150.0),
         }
     }
 }
