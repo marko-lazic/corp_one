e0d5d016b0381da6898d9e71516c282061e90c8c
Lazić <markolazic@protonmail.com>
2023-06-18 15:59:22 +0200

    Implement camera rotation and following

--- corp_client/examples/ccc/camera.rs
+++ corp_client/examples/ccc/camera.rs
@@ -1,6 +1,90 @@
-use bevy::prelude::SystemSet;
+use bevy::prelude::*;
+use bevy_dolly::prelude::{Arm, Dolly, Position, Rig, Smooth, YawPitch};
+use bevy_dolly::system::DollyUpdateSet;
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub enum CameraSet {
-    Movement,
+    Update,
+}
+
+#[derive(Component)]
+pub struct MainCamera;
+
+#[derive(Component)]
+pub struct MainCameraFollow;
+
+pub struct MainCameraPlugin;
+
+#[derive(Bundle)]
+pub struct MainCameraBundle {
+    #[bundle]
+    camera: Camera3dBundle,
+    main_camera: MainCamera,
+    rig: Rig,
+}
+
+impl Default for MainCameraBundle {
+    fn default() -> Self {
+        Self {
+            camera: Camera3dBundle::default(),
+            main_camera: MainCamera,
+            rig: Rig::builder()
+                .with(Position::new(Vec3::ZERO))
+                .with(YawPitch::new().yaw_degrees(45.0).pitch_degrees(-60.0))
+                .with(Smooth::new_position(0.3))
+                .with(Smooth::new_rotation(0.3))
+                .with(Arm::new(Vec3::Z * 4.0))
+                .build(),
+        }
+    }
+}
+
+impl Plugin for MainCameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(update_camera.in_set(CameraSet::Update))
+            .add_system(Dolly::<MainCamera>::update_active)
+            .configure_set(DollyUpdateSet.after(CameraSet::Update));
+    }
+}
+
+fn update_camera(
+    keys: Res<Input<KeyCode>>,
+    mut rig_q: Query<&mut Rig>,
+    follow_trans_q: Query<&Transform, With<MainCameraFollow>>,
+) {
+    let mut rig = rig_q.single_mut();
+    let camera_yp = rig.driver_mut::<YawPitch>();
+
+    // Rotate 90 degrees
+    if keys.just_pressed(KeyCode::Z) {
+        camera_yp.rotate_yaw_pitch(-90.0, 0.0);
+    }
+    if keys.just_pressed(KeyCode::X) {
+        camera_yp.rotate_yaw_pitch(90.0, 0.0);
+    }
+
+    if keys.pressed(KeyCode::Equals) {
+        // Zoom in
+        if let Some(arm) = rig.try_driver_mut::<Arm>() {
+            let mut xz = arm.offset;
+            xz.z = (xz.z - 1.0 * 0.5).abs();
+            arm.offset = xz;
+        }
+    }
+
+    if keys.pressed(KeyCode::Minus) {
+        // Zoom out
+        if let Some(arm) = rig.try_driver_mut::<Arm>() {
+            let mut xz = arm.offset;
+            xz.z = (xz.z + 1.0 * 0.5).abs();
+            arm.offset = xz;
+        }
+    }
+
+    // Update camera position
+    if let Some(camera_pos) = rig.try_driver_mut::<Position>() {
+        for pos in follow_trans_q.iter() {
+            camera_pos.position = pos.translation + Vec3::new(0., 1., 0.);
+        }
+    }
 }
--- corp_client/examples/ccc/ccc.rs
+++ corp_client/examples/ccc/ccc.rs
@@ -1,15 +1,13 @@
 //! Demonstrates all common configuration options,
 //! and how to modify them at runtime
 
-use std::f32::consts::TAU;
-
 use bevy::app::AppExit;
 use bevy::prelude::*;
-use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin, PanOrbitCameraSystemSet};
+use bevy_dolly::prelude::*;
 
 use corp_shared::prelude::Player;
 
-use crate::camera::CameraSet;
+use crate::camera::{CameraSet, MainCameraBundle, MainCameraFollow, MainCameraPlugin};
 use crate::character::{CharacterMovement, CharacterPlugin, CharacterSet};
 use crate::control::ControlPlugin;
 
@@ -19,22 +17,13 @@ mod control;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa::default())
         .add_plugins(DefaultPlugins)
-        .add_plugin(PanOrbitCameraPlugin)
+        .add_plugin(MainCameraPlugin)
         .add_plugin(ControlPlugin)
         .add_plugin(CharacterPlugin)
         .add_startup_system(setup)
-        .add_systems(
-            (
-                toggle_camera_controls_system,
-                camera_follow,
-                camera_rotation,
-            )
-                .in_set(CameraSet::Movement),
-        )
-        .configure_set(PanOrbitCameraSystemSet.after(CameraSet::Movement))
-        .configure_set(PanOrbitCameraSystemSet.after(CharacterSet::Movement))
-        .configure_set(CameraSet::Movement.after(CharacterSet::Movement))
+        .configure_set(CameraSet::Update.after(CharacterSet::Movement))
         .add_system(exit_game)
         .run();
 }
@@ -53,12 +42,13 @@ fn setup(
     // Player
     commands.spawn((
         PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            mesh: meshes.add(Mesh::from(shape::Box::new(0.2, 1.0, 0.2))),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..default()
         },
         Player,
+        MainCameraFollow,
         CharacterMovement::default(),
     ));
     // Light
@@ -72,100 +62,7 @@ fn setup(
         ..default()
     });
     // Camera
-    commands.spawn((
-        // Note we're setting the initial position below with alpha, beta, and radius, hence
-        // we don't set transform on the camera.
-        Camera3dBundle::default(),
-        PanOrbitCamera {
-            // Set focal point (what the camera should look at)
-            focus: Vec3::new(0.0, 1.0, 0.0),
-            target_focus: Vec3::new(0.0, 1.0, 0.0),
-            // Set the starting
-            // position, relative to focus (overrides camera's transform).
-            alpha: Some(0.0),
-            beta: Some(TAU / 8.0),
-            radius: Some(5.0),
-            // Set limits on how far in will rotate
-            alpha_upper_limit: None,
-            alpha_lower_limit: None,
-            beta_upper_limit: Some(TAU / 3.0),
-            beta_lower_limit: Some(-TAU / 3.0),
-            // Adjust sensitivity of controls
-            orbit_sensitivity: 0.5,
-            pan_sensitivity: 0.5,
-            pan_smoothness: 0.6,
-            zoom_sensitivity: 0.5,
-            // Allow the camera to go upside down
-            allow_upside_down: true,
-            // Change the controls (these match Blender)
-            button_orbit: MouseButton::Left,
-            button_pan: MouseButton::Middle,
-            modifier_pan: None,
-            // Reverse the zoom direction
-            reversed_zoom: true,
-            ..default()
-        },
-    ));
-}
-
-// Press 'T' to toggle the camera controls
-fn toggle_camera_controls_system(
-    key_input: Res<Input<KeyCode>>,
-    mut pan_orbit_query: Query<&mut PanOrbitCamera>,
-) {
-    if key_input.just_pressed(KeyCode::T) {
-        info!("T pressed");
-        for mut pan_orbit in pan_orbit_query.iter_mut() {
-            pan_orbit.enabled = !pan_orbit.enabled;
-        }
-    }
-
-    if key_input.pressed(KeyCode::Equals) {
-        for mut pan_orbit in pan_orbit_query.iter_mut() {
-            pan_orbit.radius = Some(pan_orbit.radius.unwrap() - 0.1);
-            pan_orbit.force_update = true;
-        }
-    }
-
-    if key_input.pressed(KeyCode::Minus) {
-        for mut pan_orbit in pan_orbit_query.iter_mut() {
-            pan_orbit.radius = Some(pan_orbit.radius.unwrap() + 0.1);
-            pan_orbit.force_update = true;
-        }
-    }
-}
-
-fn camera_follow(
-    player_transform_query: Query<&Transform, (With<Player>, Changed<Transform>)>,
-    mut pan_orbit_query: Query<&mut PanOrbitCamera>,
-) {
-    let Ok(player_tr) = player_transform_query.get_single() else {
-        return;
-    };
-
-    let Ok(mut pan_orbit) = pan_orbit_query.get_single_mut() else {
-        return;
-    };
-
-    pan_orbit.target_focus = player_tr.translation;
-    pan_orbit.force_update = true;
-}
-
-fn camera_rotation(
-    key_input: Res<Input<KeyCode>>,
-    mut pan_orbit_query: Query<&mut PanOrbitCamera>,
-) {
-    let Ok(mut pan_orbit) = pan_orbit_query.get_single_mut() else {
-        return;
-    };
-
-    if key_input.pressed(KeyCode::Right) {
-        pan_orbit.target_alpha = pan_orbit.alpha.unwrap_or(0.0) + 0.2;
-    }
-
-    if key_input.pressed(KeyCode::Left) {
-        pan_orbit.target_alpha = pan_orbit.alpha.unwrap_or(0.0) - 0.2;
-    }
+    commands.spawn(MainCameraBundle::default());
 }
 
 fn exit_game(keyboard_input: Res<Input<KeyCode>>, mut app_exit_events: EventWriter<AppExit>) {
